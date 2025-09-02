0f0491fc88336f6f44bee864c2fe850d236ec815
ARULKUMAR <101092540+ARULKUMAR0106@users.noreply.github.com>
2025-09-02 15:54:56 +0530

    comit

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:mode=="production"?"/dev-portfolio-dotnet/":"",
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base:"",
+
   plugins: [
     react(),
     mode === 'development' &&
